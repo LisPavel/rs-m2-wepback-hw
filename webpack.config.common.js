81fec8b0f551d81793f9f605fc13abbcd66497e9
Pavel Lisitsyn <43790794+LisPavel@users.noreply.github.com>
2024-01-06 16:08:04 +0300

    chore: add initial setup

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,11 +1,11 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+// const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
-  entry: "./index.tsx",
+  entry: "./index.jsx",
   output: {
     filename: "[name].[contenthash].bundle.js",
     path: path.resolve(__dirname, "dist"),
@@ -18,14 +18,14 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "public/index.html"),
     }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, "public/favicon.png"),
-          to: path.resolve(__dirname, "dist"),
-        },
-      ],
-    }),
+    // new CopyPlugin({
+    //   patterns: [
+    //     {
+    //       from: path.resolve(__dirname, "public/favicon.png"),
+    //       to: path.resolve(__dirname, "dist"),
+    //     },
+    //   ],
+    // }),
     new MiniCssExtractPlugin(),
   ],
   module: {
