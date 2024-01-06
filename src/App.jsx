5e4e3fa9c9a886866755885c2c8e3b888262cb5d
Pavel Lisitsyn <43790794+LisPavel@users.noreply.github.com>
2024-01-06 16:59:45 +0300

    feat: add initial template and styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import React from "react";
 export const App = () => {
   return (
     <>
-      <h1>Hello world</h1>
+      <h1 className="text-xl ">Weather sounds</h1>
+      <div className="btn-container">
+        <button className="summer"></button>
+        <button className="rain"></button>
+        <button className="winter"></button>
+      </div>
+      <input type="range" name="sound" id="sound" />
     </>
   );
 };
