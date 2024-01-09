6039c9d40e1b9fe760f0a272a3aa66d9c0a3c565
Pavel Lisitsyn <43790794+LisPavel@users.noreply.github.com>
2024-01-09 21:27:50 +0300

    feat: add playing sounds

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,50 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import rainSound from "./assets/sounds/rain.mp3";
+import summerSound from "./assets/sounds/summer.mp3";
+import winterSound from "./assets/sounds/winter.mp3";
+
+const tracks = {
+  rain: rainSound,
+  summer: summerSound,
+  winter: winterSound,
+};
 
 export const App = () => {
+  const [trackName, setTrackName] = useState("summer");
+  const [currentTrack, setCurrentTrack] = useState(tracks[trackName]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const audioRef = useRef();
+
+  const playTrack = (newTrackName) => {
+    if (newTrackName === trackName) {
+      setIsPlaying((prev) => !prev);
+    } else {
+      setIsPlaying(true);
+      setTrackName(newTrackName);
+      setCurrentTrack(tracks[newTrackName]);
+    }
+  };
+
+  useEffect(() => {
+    console.log(isPlaying);
+    if (isPlaying) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  }, [isPlaying, audioRef, currentTrack]);
+
   return (
     <>
       <h1 className="text-xl ">Weather sounds</h1>
       <div className="btn-container">
-        <button className="summer"></button>
-        <button className="rain"></button>
-        <button className="winter"></button>
+        <button className="summer" onClick={() => playTrack("summer")}></button>
+        <button className="rain" onClick={() => playTrack("rain")}></button>
+        <button className="winter" onClick={() => playTrack("winter")}></button>
       </div>
-      <input type="range" name="sound" id="sound" />
+      <audio src={currentTrack} ref={audioRef} />
+      <input type="range" name="sound" id="sound" min={0} max={100} />
     </>
   );
 };
