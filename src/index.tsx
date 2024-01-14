0ad30e390a247cb90e8512659ff2bea024c072d8
Pavel Lisitsyn <43790794+LisPavel@users.noreply.github.com>
2024-01-14 12:49:02 +0300

    feat(index): rewrite with ts

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,6 @@ import { App } from "./App";
 
 import "./index.scss";
 
-const root = createRoot(document.getElementById("app"));
+const root = createRoot(document.getElementById("app") as HTMLElement);
 
 root.render(<App />);
